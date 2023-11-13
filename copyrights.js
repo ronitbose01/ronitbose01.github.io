eb3d285c3721cac8aa2d83e9f0f5eb71e7c25c9f
Coding Tripura <82285375+ronitbose01@users.noreply.github.com>
2023-11-13 12:38:33 +0530

    Create copyrights.js

---
+++ copyrights.js
@@ -0,0 +1,16 @@
+       const consoleStyles = `
+  font-family: 'Helvetica Neue', sans-serif;
+  font-size: 18px;
+  font-weight: bold;
+  background: linear-gradient(to right, #3498db, #6dd5fa);
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+`;
+
+            console.log(
+                `%c Developed with ❤️ by CodingTripura Team`,
+                `${consoleStyles} cursor: pointer;`,
+            );
+            console.info('🌐 Explore our software at https://www.codingtripura.com for cutting-edge technology and seamless performance.');
